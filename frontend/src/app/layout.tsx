e1ce16bd2106111f2aab50de3eda25e2d840b0d8
sharoonm99 <you@example.comsharunedv@gmail.com>
2025-07-16 15:10:05 +0530

    frontend folder structure

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,15 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export const metadata = {
+  title: 'Product Management',
+};
